1b0c4773993ea85560317d53c4005e8d221ee316
zanyfelix <zanyfelix@gmail.com>
2025-06-15 22:30:18 +0900

    first commit

---
+++ src/main/java/com/mbiz/yearbook/YearbookApplication.java
@@ -0,0 +1,13 @@
+package com.mbiz.yearbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YearbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YearbookApplication.class, args);
+	}
+
+}
