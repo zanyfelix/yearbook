ba675936a7044c78df8d056e35100765c5196436
zanyfelix <zanyfelix@gmail.com>
2025-08-05 07:41:55 +0900

    수정 및 저장

--- src/main/resources/static/js/frame/FrameManager.js
+++ src/main/resources/static/js/frame/FrameManager.js
@@ -22,7 +22,7 @@ class FrameManager {
 
 		const placeholderLink = $('<a href="#" class="place-image-here-link">Place Image Here</a>').css({
 			color: 'white', textDecoration: 'underline',
-			fontSize: '14px', fontWeight: 'bold',
+			fontSize: '12px', fontWeight: 'bold',
 			textAlign: 'center', display: 'block',
 			position: 'relative',
 			cursor: 'pointer'  // 포인터 커서 추가
