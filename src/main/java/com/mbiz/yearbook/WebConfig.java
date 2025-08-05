be127e9e675ed84386e4870ff53fafd7ec81a3ac
zanyfelix <zanyfelix@gmail.com>
2025-08-06 08:14:43 +0900

    세션 값 설정

--- src/main/java/com/mbiz/yearbook/WebConfig.java
+++ src/main/java/com/mbiz/yearbook/WebConfig.java
@@ -1,12 +1,32 @@
 package com.mbiz.yearbook;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	
+	@Autowired
+    private SessionInterceptor sessionInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(sessionInterceptor)
+                .addPathPatterns("/**")  // 모든 경로에 적용
+                .excludePathPatterns(
+                    "/login",           // 로그인 페이지
+                    "/",               // 루트 경로
+                    "/css/**",         // CSS 파일들
+                    "/js/**",          // JavaScript 파일들
+                    "/images/**",      // 이미지 파일들
+                    "/static/**",      // 정적 리소스들
+                    "/favicon.ico"     // 파비콘
+                );
+    }
 	
 	@Value("${file.upload-dir}")
     private String uploadPath;
