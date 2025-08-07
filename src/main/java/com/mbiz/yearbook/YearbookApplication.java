713ab99bf0aa8a7b9c4b442caf230ad8cf5763d5
zanyfelix <zanyfelix@gmail.com>
2025-08-07 18:07:28 +0900

    배포설정 완료

--- src/main/java/com/mbiz/yearbook/YearbookApplication.java
+++ src/main/java/com/mbiz/yearbook/YearbookApplication.java
@@ -2,12 +2,18 @@ package com.mbiz.yearbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class YearbookApplication {
+public class YearbookApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(YearbookApplication.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(YearbookApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(YearbookApplication.class, args);
+    }
 }
